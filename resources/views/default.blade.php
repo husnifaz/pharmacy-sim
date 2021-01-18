1b064ea81193f64a35d9635ca64c1d45183f5b7c
Muhammad Husni Farid <husnifa02@gmail.com>
2021-01-18 09:52:18 +0700

    initial commit

---
+++ resources/views/default.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>@yield('title', 'Head')</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('css/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+            page. However, you can choose any other skin. Make sure you
+            apply the skin class to the body tag so the changes take effect. -->
+    <link rel="stylesheet" href="{{asset('css/skin-blue.min.css')}}">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+    <div id="header">
+        @include('parts.header')
+    </div>
+    <div id="menu">
+        @include('parts.menu')
+    </div>
+    <div id="content"></div>
+</body>
+<!-- jQuery 3 -->
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('js/adminlte.min.js')}}"></script>
+</html>
