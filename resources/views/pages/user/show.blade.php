7c5863127528dc555f87b38646d6a0dfc6c2268c
Muhammad Husni Farid <husnifa02@gmail.com>
2021-06-10 22:31:36 +0700

    update menu management

--- resources/views/pages/user/show.blade.php
+++ resources/views/pages/user/show.blade.php
@@ -1,6 +1,6 @@
 @extends('default')
 @section('content_header')
-  <x-title-bar title="Detail User"/>
+<x-title-bar title="Detail User" />
 @endsection
 @section('content')
 <div class="row">
@@ -8,24 +8,23 @@
     <div class="box box-primary">
       <div class="box-body">
         <div class="row group-detail">
-            <div class="col-md-3">
-                <label>Nama User</label>
-                <p>{{$model->name}}</p>
-            </div>
-            <div class="col-md-3">
-                <label>Email</label>
-                <p>{{$model->email}}</p>
-            </div>
+          <div class="col-md-3">
+            <label>Nama User</label>
+            <p>{{$model->name}}</p>
+          </div>
+          <div class="col-md-3">
+            <label>Email</label>
+            <p>{{$model->email}}</p>
+          </div>
         </div>
         <div class="row group-detail">
-            <div class="col-md-3">
-                <label>Hak Akses</label>
-                <p>{{}}</p>
-            </div>
+          <div class="col-md-3">
+            <label>Hak Akses</label>
+            <p></p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
 @endsection
-
